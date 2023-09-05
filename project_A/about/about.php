4d07fa2aa33a759e1e37ac8faaa4420814446484
Josot <joost@vanewijk.info>
2023-09-05 21:30:29 +0200

    change from 'project A'

--- project_A/about/about.php
+++ project_A/about/about.php
@@ -22,7 +22,7 @@
         <label for="check" class="checkbtn">
             <i class="fas fa-bars"></i>
         </label>
-        <a href="../index.php"><label class="logo">Project A</label></a>
+        <a href="../index.php"><label class="logo">Portfolio</label></a>
         <ul>
             <li><a href="../index.php">Home</a></li>
             <li><a href="../project_pagina/project_pagina.php">Projects</a></li>
--- project_A/project_pagina/project_pagina.php
+++ project_A/project_pagina/project_pagina.php
@@ -11,7 +11,7 @@ require_once '../../db/connection.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../style/style.css">
-    <title>Index</title>
+    <title>Projects</title>
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
